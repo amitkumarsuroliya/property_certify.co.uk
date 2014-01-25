7a4faad6e9560fc45fee679e0b7bdda239b3af71
Akash Soti <akujgd@gmail.com>
2014-01-25 10:34:36 +0530

    initial

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+PropertyCertifyCoUk::Application.config.secret_key_base = '866eab31c9ef49468c9dddf7f09b9dd2a0967706c0826ee9b16465129c209794c11c98abb4dea8f57529dc7063caf907b5172a585d751ec80c027d977fd2c78e'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,3 @@
+# Be sure to restart your server when you modify this file.
+
+PropertyCertifyCoUk::Application.config.session_store :cookie_store, key: '_property_certify_co_uk_session'
