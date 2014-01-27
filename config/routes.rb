5985b8f0c22f7c3b52a437d2742a72946f562fa1
Akash Soti <akashsoti@gmail.com>
2014-01-27 16:28:16 +0530

    remove unnecessary controller

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,6 @@
 PropertyCertifyCoUk::Application.routes.draw do
   
-  get "about/index"
+  get "about", to: 'welcome#about'
   
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
